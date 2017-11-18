140cb9bfe8248af20c6ff72e0ec661fc4c590d56
jxq0816 <jxq0816@126.com>
2017-11-18 20:28:57 +0800

    human 代码注释

--- src/jzombies/Human.java
+++ src/jzombies/Human.java
@@ -28,22 +28,21 @@ public class Human {
   }
   
 
-@Watch(watcheeClassName = "jzombies.Zombie",
-      watcheeFieldNames = "moved",
-      query = "within_moore 1",
-      whenToTrigger = WatcherTriggerSchedule.IMMEDIATE)
+  @Watch(watcheeClassName = "jzombies.Zombie",watcheeFieldNames = "moved",query = "within_moore 1",whenToTrigger = WatcherTriggerSchedule.IMMEDIATE)
   public void run(){
-    GridPoint pt = grid.getLocation(this);
-    GridCellNgh<Zombie> nghCreator = new GridCellNgh<Zombie>(grid, pt, Zombie.class, 1, 1);
-    List<GridCell<Zombie>> gridCells = nghCreator.getNeighborhood(true);
-    SimUtilities.shuffle(gridCells, RandomHelper.getUniform());
+    GridPoint pt = grid.getLocation(this);//获得人类所在点
+    //获得人类现在所在点周围的网格单元，并获得单元周围Zombie的数量
+    GridCellNgh<Zombie> nghCreator = new GridCellNgh<Zombie>(grid, pt, Zombie.class, 1, 1);//因为是二维的，最后两个值是x,y方向观察的范围
+    List<GridCell<Zombie>> gridCells = nghCreator.getNeighborhood(true);//网格单元列表
+    SimUtilities.shuffle(gridCells, RandomHelper.getUniform());//随机打乱列表
     
-    GridPoint pointWithLeastZombies = null;
-    int minCount = Integer.MAX_VALUE;
-    for(GridCell<Zombie> cell : gridCells){
+    GridPoint pointWithLeastZombies = null;//目标点：僵尸最少的点
+    int minCount = Integer.MAX_VALUE;//最小值初始化
+    for(GridCell<Zombie> cell : gridCells){//遍历网格单元列表
+    	//加入网格单元的僵尸数量，比当前最小值小，则更新
       if(cell.size()<minCount){
-        pointWithLeastZombies = cell.getPoint();
-        minCount = cell.size();
+        pointWithLeastZombies = cell.getPoint();//更新目标点
+        minCount = cell.size();//僵尸最小值更新
       }
     }
     
